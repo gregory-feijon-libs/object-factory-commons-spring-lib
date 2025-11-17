3b9eb5176606af2e4b3743037bb7f5d8bfdfd108
Gregory Feijon <gregory.feijon@outlook.com>
2025-11-17 19:07:35 -0300

    FIRST COMMIT

---
+++ src/test/java/io/github/gregoryfeijon/object/factory/commons/ObjectFactoryCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gregoryfeijon.object.factory.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectFactoryCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
