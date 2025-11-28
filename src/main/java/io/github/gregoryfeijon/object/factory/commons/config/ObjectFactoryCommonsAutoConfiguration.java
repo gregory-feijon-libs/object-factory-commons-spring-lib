5cf6cbf2714e4368e357411177a8dd6d41299fc3
Gregory Feijon <gregory.feijon@outlook.com>
2025-11-28 18:40:34 -0300

    [UPDATE] Adicionando AutoConfiguration

---
+++ src/main/java/io/github/gregoryfeijon/object/factory/commons/config/ObjectFactoryCommonsAutoConfiguration.java
@@ -0,0 +1,13 @@
+package io.github.gregoryfeijon.object.factory.commons.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Auto-configuração do Object Factory Commons.
+ * Carregada automaticamente pelo Spring Boot.
+ */
+@AutoConfiguration
+@ComponentScan(basePackages = "io.github.gregoryfeijon.object.factory.commons")
+public class ObjectFactoryCommonsAutoConfiguration {
+}
